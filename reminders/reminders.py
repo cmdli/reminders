b9ba8ae6d821b1c0734ebd1729ca30b393809fef
Chris de la Iglesia <christopher.delaiglesia@gmail.com>
2017-11-30 16:16:49 -0800

    Added sqlite database code

--- reminders/reminders.py
+++ reminders/reminders.py
@@ -1,11 +1,44 @@
-
-from flask import Flask
+import os
+import sqlite3
+from flask import Flask, g, request, redirect, url_for
 
 app = Flask(__name__)
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path,'reminders.db'),
+    DEBUG=True
+))
 
-@app.route("/")
+def connect_db():
+    db = sqlite3.connect(app.config['DATABASE'])
+    db.row_factory = sqlite3.Row
+    return db
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
 def hello_world():
-    return "<!DOCTYPE html><html><body>Hello world!</body></html>"
+    db = get_db()
+    query = db.execute('select phone_number,reminder_text,reminder_time from reminders')
+    reminders = query.fetchall()
+    print reminders
+    return str([str(r) for r in reminders])
+
+@app.route('/add', methods=['POST'])
+def add():
+    db = get_db()
+    db.execute('insert into reminders (phone_number,reminder_text,reminder_time)' +
+               'values (?,?,?)',
+               [request.form['number'],request.form['text'],request.form['time']])
+    db.commit()
+    return redirect(url_for('hello_world'))
 
 if __name__ == "__main__":
     app.run()
