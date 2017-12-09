039893a6bab171ab87b8d1abd01f58aa5e5964ca
Chris de la Iglesia <christopher.delaiglesia@gmail.com>
2017-12-08 23:10:31 -0800

    Added code to send reminders when receiving a request to /send_reminders

--- reminders/reminders.py
+++ reminders/reminders.py
@@ -1,5 +1,4 @@
-import os
-import sqlite3
+import os, time, sqlite3
 from flask import Flask, g, request, redirect, url_for
 from twilio.rest import Client as TwilioClient
 from twilio.twiml.messaging_response import MessagingResponse, Message
@@ -56,7 +55,7 @@ def get_reminders():
 
 @app.route('/add', methods=['POST'])
 def add():
-    add_message(request.form['number'],
+    add_reminder(request.form['number'],
                 request.form['text'],
                 request.form['time'])
     return redirect(url_for('get_reminders'))
@@ -69,14 +68,16 @@ def usage():
 @app.route('/receive', methods=['POST'])
 def receive():
     try:
+        # Body form - <text> <# of seconds from now>
         body = request.values['Body']
         number = request.values['From']
         values = body.split(' ')
-        text,time = values[0],int(values[1])
-        add_reminder(number,values[0],int(values[1]))
+        text,expiration = values[0],int(values[1])
+        reminder_time = expiration + int(time.time()) # Add current time
+        add_reminder(number,text,reminder_time)
         resp = MessagingResponse()
         resp.message("Added reminder for '" + text +
-                     "' in " + str(time) + " seconds")
+                     "' in " + str(expiration) + " seconds")
         return str(resp)
     except (ValueError,KeyError):
         return usage()
@@ -93,7 +94,28 @@ def send_message(to,body):
         to=to,
         from_=app.config['TWILIO_NUMBER'],
         body=body
-    )    
+    )
+
+@app.route('/send_reminders', methods=['GET'])
+def send_reminders():
+    db = get_db()
+    cursor = db.execute('select * from reminders')
+    reminders = cursor.fetchall()
+    client = TwilioClient(app.config['TWILIO_SID'],app.config['TWILIO_AUTH_TOKEN'])
+    sent = []
+    current = int(time.time())
+    for reminder in reminders:
+        if reminder['reminder_time'] < current:
+            client.api.account.messages.create(
+                to=reminder['phone_number'],
+                from_=app.config['TWILIO_NUMBER'],
+                body=reminder['reminder_text']
+            )
+            sent.append(reminder)
+    for reminder in sent:
+        db.execute('delete from reminders where id=(?)', [reminder['id']])
+    db.commit()
+    return str([tuple(r) for r in sent])
 
 @app.route('/send', methods=['POST'])
 def send():
